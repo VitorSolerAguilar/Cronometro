49210a0e2f807f98a2f9f89ddc29acd4aba0521b
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2022-09-06 18:44:38 -0300

    Primeiro commit

---
+++ Cronometro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Segundos = 0;
+
+                Console.Write("Digite a quantidade de tempo em segundos: ");
+                Segundos = int.Parse(Console.ReadLine());
+
+                for (int i = Segundos - 1; i < Segundos; i--)
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Segundos: " + i);
+
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Tempo esgotado.");
+                        Environment.Exit(0);
+                    }
+                }
+            }catch(Exception Erro)
+            {
+                Console.WriteLine(Erro.Message);
+            }
+        }
+    }
+}
